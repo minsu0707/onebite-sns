0fb260ceac56ebbbeff0aaa0dba68031acb5eddb
민수 <mandoo4137@gmail.com>
2025-12-10 11:30:09 +0900

    feat : forget-password

---
+++ src/hooks/mutations/use-update-password.ts
@@ -0,0 +1,15 @@
+import { updatePasword } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+
+export function useUpdatePassword(callbacks?: UseMutationCallback) {
+  return useMutation({
+    mutationFn: updatePasword,
+    onSuccess: () => {
+      if (callbacks?.onSuccess) callbacks.onSuccess();
+    },
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+  });
+}
